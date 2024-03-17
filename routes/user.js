78f52243ca0a56446d571f1f6b069447dc3c6bb2
Pratik singh <pratiksingh59165@gmail.com>
2024-03-17 23:49:16 +0530

    Initial commit

--- routes/user.js
+++ routes/user.js
@@ -158,6 +158,7 @@ router.get("/:id/profile", isLoggedIn, async(req, res, next) => {
     let {id} = req.params;
     let posts = await Post.find({creater: id});
     let user = await User.findOne({_id: id});
+    console.log(posts[0])
     res.render("users/userPage.ejs", {user, posts});
 });
 
